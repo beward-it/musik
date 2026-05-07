900a52dfa506e81599eac926af1801a15addf1a6
beward-it <anotheronemailkotopom@gmail.com>
2026-05-07 21:20:08 +0500

    modified:   main.py

--- main.py
+++ main.py
@@ -25,32 +25,13 @@ musik.remove('README.md')
 musik.remove('.git')
 mode = int(input("Выбери режим 1 - повторение, 2 - проверка знаний, 3 - экзамен: "))
 i = 0
-print(len(musik))
+result = 0
 random.shuffle(musik)
 while True:
     if i == len(musik):
         random.shuffle(musik)
         print("Все 20 произведений прослушаны")
-        i = 0
-    player.init()
-    sound = musik[i]
-    print(sound)
-    zvuk = player.Sound(sound)
-    zvuk.play()
-    number, autor, name = sound.split("_")
-    if mode == 1:
-        print(f"Это номер {number}, автор - {autor}, название - {name}.")
-        input()
-    elif mode == 2:
-        if number == input("Номер произведения: "):
-            print("Верно!")
-        else:
-            print(f"Неверно! Это номер {number}, автор - {autor}, название - {name}.")
-    else:
-        result = 0
-        if number == input("Номер произведения: "):
-            result += 1
-        if i == 20:
+        if mode == 3:
             if 0.75 > result / 20 >= 0.5:
                 grade = 3
             elif result / 20 < 0.5:
@@ -60,7 +41,22 @@ while True:
             else:
                 grade = 4
             print(f"Ваш результат {result} / 20. Ваша оценка - {grade}.")
-            break
+            result = 0
+    player.init()
+    sound = musik[i]
+    zvuk = player.Sound(sound)
+    zvuk.play()
+    number, autor, name = sound.split("_")
+    if mode == 1:
+        print(f"Это номер {number}, автор - {autor}, название - {name}.")
+        input()
+    elif mode in (2, 3):
+        if number == input("Номер произведения: "):
+            print("Верно!")
+            if mode == 3:
+                result += 1
+        else:
+            print(f"Неверно! Это номер {number}, автор - {autor}, название - {name}.")
     zvuk.stop()
     i += 1
 """@wind.event
